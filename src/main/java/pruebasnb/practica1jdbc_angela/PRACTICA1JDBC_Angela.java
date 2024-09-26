05e98e01113a70dcd7ccddd3f4f629ce7bfdb4f8
angela <angelasaezgim12@gmail.com>
2024-09-26 10:27:36 +0200

    1

---
+++ src/main/java/pruebasnb/practica1jdbc_angela/PRACTICA1JDBC_Angela.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package pruebasnb.practica1jdbc_angela;
+
+/**
+ *
+ * @author angsaegim
+ */
+public class PRACTICA1JDBC_Angela {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! 1");
+    }
+}
