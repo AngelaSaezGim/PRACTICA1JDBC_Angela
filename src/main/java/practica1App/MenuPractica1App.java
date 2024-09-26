c96070dde2dd1a953d3d9749acbf268ad138ac8f
angela <angelasaezgim12@gmail.com>
2024-09-26 10:45:07 +0200

    2

--- src/main/java/practica1App/MenuPractica1App.java
+++ src/main/java/practica1App/MenuPractica1App.java
@@ -3,13 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package pruebasnb.practica1jdbc_angela;
+package practica1App;
 
 /**
  *
  * @author angsaegim
  */
-public class PRACTICA1JDBC_Angela {
+public class MenuPractica1App {
 
     public static void main(String[] args) {
         System.out.println("Hello World! 2");
---
+++ src/main/java/practica1DataAccess/DataAccessObject.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica1DataAccess;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author angsaegim
+ */
+public class DataAccessObject {
+    
+    protected Connection cnt;
+    
+    DataAccessObject(Connection cnt){
+        if(cnt==null)
+            throw new IllegalArgumentException("Conexión obligatoria");
+        this.cnt = cnt;
+    }
+    
+}
